2413f41ed68856a64c763d82525c12648f1b3b28
Hans <hansgsingh@yahoo.com>
2021-09-06 23:23:59 +0800

    table chat and styles

--- app/package/sockets.py
+++ app/package/sockets.py
@@ -3,6 +3,7 @@ from flask_login import current_user
 from flask_socketio import emit, join_room, leave_room
 
 SHOW_SERVER_ROOM_USERS = []
+SHOW_ROOM_USERS = []
 USERS_WHO_JUST_JOINED = []
 
 
@@ -42,10 +43,60 @@ def _():
     emit('connected_users_count', {'users_count': len(SHOW_SERVER_ROOM_USERS)}, to='show_server_room', broadcast=True)
 
 
-        
-
 
 @sio.on('new_msg', namespace='/show_server')
 def _(data):
     print(current_user.username + ' sent a message on show_server_room')
     emit('append_msg', {'username': current_user.username, 'msg': data['msg']}, to='show_server_room', include_self=False)
+
+
+
+
+
+
+
+
+
+# TABLE ROOM
+
+@sio.on('user_just_joined', namespace='/show_room')
+def _():
+    print('60 SECONDS HAS PAST')
+    USERS_WHO_JUST_JOINED.remove(current_user.username)
+
+
+@sio.on('connect', namespace='/show_room')
+def _():
+    print(f'{current_user.username} connected')
+
+
+
+@sio.on('disconnect', namespace='/show_room')
+def _():
+    print(current_user.username + ' left the show_room')
+    leave_room('show_room')
+    SHOW_ROOM_USERS.remove(current_user.username)
+    print(SHOW_ROOM_USERS)
+    emit('connected_users_count', {'users_count': len(SHOW_ROOM_USERS)}, to='show_room', broadcast=True)
+    emit('disconnected', {'username': current_user.username}, to='show_room', broadcast=True)
+
+
+
+@sio.on('join', namespace='/show_room')
+def _():
+    if current_user.username not in SHOW_ROOM_USERS:
+        join_room('show_room')
+        SHOW_ROOM_USERS.append(current_user.username)
+
+    print(f"{current_user.username} joined show_room")
+    USERS_WHO_JUST_JOINED.append(current_user.username)
+    emit('joined', {'username': current_user.username}, to='show_room', include_self=False)
+
+    emit('connected_users_count', {'users_count': len(SHOW_ROOM_USERS)}, to='show_room', broadcast=True)
+
+
+
+@sio.on('new_msg', namespace='/show_room')
+def _(data):
+    print(current_user.username + ' sent a message on show_room')
+    emit('append_msg', {'username': current_user.username, 'msg': data['msg']}, to='show_room', include_self=False)
---
+++ app/package/static/js/show_room.js
@@ -0,0 +1,66 @@
+const socket = io('/show_room')
+
+socket.on('connect', ()=>{
+    console.log(socket.id + ' connected')
+
+    socket.emit('join')
+    setTimeout(()=>{
+        socket.emit('user_just_joined')
+    }, 120000)
+})
+
+socket.on('connected_users_count', (data)=>{
+    let users_count_element = document.getElementById('active-users-count')
+    users_count_element.innerHTML = 'CONNECTED USERS: ' + data.users_count
+})
+
+
+socket.on('disconnected', (user) => {
+    let msg_div = '<div class="d-flex justify-content-center">'
+    let msg_p = '-------------- ' + user.username + '</b> left the room --------------'
+    let msg_end_div = '</div>'
+    
+    let msg_html = msg_div + msg_p + msg_end_div
+    msg_container.innerHTML += msg_html
+})
+
+
+socket.on('joined', (user)=>{
+    console.log(user.username + ' connected')
+
+    let msg_div = '<div class="d-flex justify-content-center">'
+    let msg_p = '-------------- ' + user.username + '</b> joined the room --------------'
+    let msg_end_div = '</div>'
+    
+    let msg_html = msg_div + msg_p + msg_end_div
+    msg_container.innerHTML += msg_html
+})
+
+socket.on('append_msg', (data) => {
+    let msg_container = document.getElementById('msg_container')
+
+    let msg_div = '<div class="d-flex flex-row">'
+    let msg_p = '<p><b>' + data.username + '</b>: ' + data.msg + '</p>'
+    let msg_end_div = '</div>'
+    
+    let msg_html = msg_div + msg_p + msg_end_div
+    msg_container.innerHTML += msg_html
+})
+
+
+function checkKey(e){
+    var enterKey = 13;
+    if (e.which == enterKey){
+        let msg_input = document.getElementById('msg')
+        let msg_container = document.getElementById('msg_container')
+
+        let msg_div = '<div class="d-flex flex-row-reverse">'
+        let msg_p = '<p><b>You<b>: ' + msg_input.value + '</p>'
+        let msg_end_div = '</div>'
+        
+        let msg_html = msg_div + msg_p + msg_end_div
+        msg_container.innerHTML += msg_html
+        socket.emit('new_msg', {msg: msg_input.value})
+        msg_input.value = '';
+    }
+}
